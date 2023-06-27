d6616ee3b0a698870001fcf38dceff803e3c7886
lifeablestuff <ianshi1111@gmail.com>
2023-06-27 14:25:51 -0700

    Update file_reader.py

--- file_reader.py
+++ file_reader.py
@@ -9,7 +9,7 @@ class DoStuff:
 
     def filter_events(self, offense, month_range, date_range):
         newshit = []
-        
+        count = {}
         if offense is not None:
             for record in self.stats:
                 if record["TYPE"] == offense.upper():
@@ -21,8 +21,12 @@ class DoStuff:
                     if int(record["MONTH"]) <= month_range[1] and int(record["MONTH"]) >= month_range[0]:
                         if int(record["DAY"]) <= date_range[1] and int(record["DAY"]) >= date_range[0]:
                             newshit.append(record)
-        
-        return newshit
+        for x in newshit:
+            if x["TYPE"] not in count:
+                count[x["TYPE"]] = 1
+            else:
+                count[x["TYPE"]] +=1
+        return newshit, count
 
 # Get the current date
 current_date = datetime.date.today().day
@@ -30,7 +34,7 @@ current_date = datetime.date.today().day
 # Get the current month
 current_month = datetime.date.today().month
 
-# Read CSV file (current path specifically on my PC)
+# Read CSV file
 data = []
 with open("crimedata_csv_AllNeighbourhoods_2023.csv", "r", newline="") as file:
     reader = csv.DictReader(file)
@@ -38,5 +42,6 @@ with open("crimedata_csv_AllNeighbourhoods_2023.csv", "r", newline="") as file:
         data.append(row)
 
 inst = DoStuff(data, current_date, current_month)
-results = inst.filter_events('Mischief', [1, 3], [4, 6])
+results = inst.filter_events('Mischief', [1, 3], [4, 6]) 
+# Arguments as follows: Type of offense(case fucking sensitive in str), Month range in list(from what month to what month and items must be int), date range(from what day in the month to what date and items must be int)
 print(results)
